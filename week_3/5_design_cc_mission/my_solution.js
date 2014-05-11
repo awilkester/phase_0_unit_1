d99590bc2e51f9c677903485227e6c4c48b5daf4
austinhay <hayaustin@gmail.com>
2014-05-11 16:06:09 -0700

    Finished my design mission.

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -1,37 +1,52 @@
-// I worked [by myself, with:] on this challenge
+// I worked by my lonesome on this challenge.
+
+// Mission Title:
+
+// DBC | Happy Hour in NYC Challenge!
 
 // Your mission description:
-// 
-// 
-// 
+
+/* Your mission is to walk through the streets of NYC with your DBC pair 
+partner to the bar to get a beer. Your character is YOU! (Maybe we can pull in your avatar 
+from Facebook or another social media site). You have 2 minutes to walk through the NYC 
+streets (designed to be a maze) to the nearest bar. If you run out of time an alert will 
+play that saying that Happy Hour is over and you have a new DBC project that you have to finish!
+/*
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// 1) Define the GUI game play area play as a simple box with a heigh and width that is 50%.
+// 2) In order to creat the maze we will design horizontal and vertical columns by using div elements. 
+// 3) Each div will have unique chracteristics with borders and no pass barriers that will create the NYC maze structure.
+// 4) Use text elements to label streets, subways and other objects.
+// 4) For simplicity we will have three main object: us, our pairing partner, and the bar.
+// 5) Add the properties of name & position to all of our variables.
+// 6) Add property of reaching the bar. (game over)
+// 7) Start your avatar and your pairing partner at the DBC headquarter positioned on the bottom right hand corner, on coordinates (x,y)
+// 8) Start DBC headquarters on coordinates (x,y)
+// 9) Start NYC bar on coordinates (x,y)
+// 9) Move avatar along different coordinate pathways (x,y) (imagine both avatar plus pairing partner move in a synchronized fashion, so it really acts as one object)
 
-// Initial Code
+// Initial Code:
 
+// this.DBC(avatar);
+// this.move(avatar);
+// this.bar(avatar);
 
+// Refactored Code:
 
+// No refactoring to report! (I bet there is, but after 30 mins of considering it I am not sure how I can make this better! [:<]    )
 
+// Reflection:
 
+/* I struggled a lot with this challenge and I found it very confusing/difficult to understand what level of ddetail we should go into 
+for the initial descriptiona and the pseudo code. Part of this is that I don't fully understand Javascript yet. For some reason I am struggling 
+with this material a lot more than I did with Ruby or HTML/CSS. I have already bought Eloquent Javascript and plan on going through this material 
+more in-depth on my free time so I am not behind when DBC starts up.
 
-// Refactored Code
+In particular, I also struggled with understanding how best to use properties, objects and values. My pseudo code is not exactly what I want it to be, 
+but I also recognize that through time and more concentraiton on the material I will be able to take a bigger deep dive into this element of my education.
 
+Overall this challenge was a great experience and extremely humbling, as it was the first time this unit where I felt a little frustrated and panicked about 
+finishing the material. I am happy that I was able to calm down, perform as best I could and find a solution that fits my learning needs (by buying the JS book, which was recommended by my instrcutor from GPS 2.1!)
 
-
-
-
-
-// Reflection
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+*/
